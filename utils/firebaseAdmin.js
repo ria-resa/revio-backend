7a4dd4cfe220479564016f7929873f8324b2fa47
mary-therese <mariatheresayu17@gmail.com>
2025-11-06 14:23:15 +0800

    for thsake of commit 4?

--- utils/firebaseAdmin.js
+++ utils/firebaseAdmin.js
@@ -6,12 +6,9 @@ import { dirname } from "path";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 dotenv.config();
 
-
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
 
 if (!admin.apps.length) {
   admin.initializeApp({
